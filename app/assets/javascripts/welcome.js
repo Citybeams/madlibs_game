7c546a447538545d3980b042e0b2c19ca325f8a6
citybeams <nicklohr1@gmail.com>
2014-06-05 09:46:04 -0400

    finished styling and functionality

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -9,19 +9,33 @@ var prompts = [
     'Type a noun'
     ];
 
+// holds the answers
+var answers = [];
+
 //keep track of current prompt we're on
 var currentPrompt = 0;
 
 //A function that will call the nextPrompt
 var nextPrompt = function(){
+  if (currentPrompt != 0) {
+    answers.push($('input').val());
+  }
+
   if (currentPrompt < prompts.length){
   // put first prompt in all html elements with class prompt
-  $('.prompt').html(prompts[currentPrompt]);
+  $('.prompt').html(prompts[currentPrompt] + '<br><input type="text">');
   currentPrompt++;
   } else {
-    //puts a new message in the html
-    $('.prompt').html("that's all for now!");
+    //run showFinal
+    showFinal();
   }
+}
+
+//puts users input into html
+var showFinal =  function() {
+$('.prompt').html('This is the story of <span class="fill">'+answers[0]+'</span> and the <span class="fill">'+answers[1]+'</span> <span class="fill">'+answers[2]+'</span>.');
+   //hides button when over
+  $('button').hide();
 }
 
 //run nextPrompt function when button is clicked
